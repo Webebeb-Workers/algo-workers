3035494551c7a7f6369850550ad70492a8bda485
유미라 <yoomira@yoomira-MacBookAir.local>
2025-02-04 09:49:27 +0900

    Refactor 나무 섭지

--- Softeer/20250203_나무_섭지/ssum1ra.py
+++ Softeer/20250203_나무_섭지/ssum1ra.py
@@ -2,7 +2,7 @@ import sys
 from collections import deque
 
 def bfs(sx, sy):
-    global n, m, grid, dist
+    dist = [[float('inf')] * m for _ in range(n)]
     
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
@@ -12,6 +12,10 @@ def bfs(sx, sy):
     
     while(q):
         x, y = q.popleft()
+
+        if((x, y) == end):
+            return dist[x][y]
+        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
@@ -20,6 +24,7 @@ def bfs(sx, sy):
                 if dist[nx][ny] == float('inf'):
                     dist[nx][ny] = dist[x][y] + 1
                     q.append((nx, ny))
+    return float('inf')
 
 n, m = map(int, input().split())
 grid = [list(input()) for _ in range(n)]
@@ -35,16 +40,15 @@ for i in range(n):
         elif grid[i][j] == 'G':
             ghosts.append((i,j))
 
-dist = [[float('inf')] * m for _ in range(n)]
-bfs(start[0], start[1])
+man_dist = bfs(start[0], start[1])
 
 ans = 'Yes'
-if dist[end[0]][end[1]] == float('inf'):
+if man_dist == float('inf'):
     ans = 'No'
 else:
     for (gx, gy) in ghosts:
         ghost_dist = abs(end[0] - gx) + abs(end[1] - gy)
-        if dist[end[0]][end[1]] >= ghost_dist:
+        if man_dist >= ghost_dist:
             ans = 'No'
             break
     
