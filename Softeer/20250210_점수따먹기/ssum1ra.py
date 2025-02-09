2ea0959ecee4bd0191a62cbd23275e1d6c55882a
유미라 <yoomira@yoomira-MacBookAir.local>
2025-02-10 01:11:47 +0900

    Refactor 점수따먹기

--- Softeer/20250210_점수따먹기/ssum1ra.py
+++ Softeer/20250210_점수따먹기/ssum1ra.py
@@ -1,17 +1,15 @@
 n, m = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(n)]
-
-cum = [[0] * (m+1) for _ in range(n+1)]
-
-for i in range(1, n+1):
-    for j in range(1, m+1):
-        cum[i][j] = matrix[i-1][j-1] + cum[i-1][j] + cum[i][j-1] - cum[i-1][j-1]
+matrix = [[0]*(m+1)] + [([0] + list(map(int, input().split()))) for _ in range(n)]
 
 res = float('-inf')
+
 for i in range(1, n+1):
-    for j in range(1, m+1):
-        for x in range(i, 0, -1):
-            for y in range(j, 0, -1):
-                res = max(res, cum[i][j] + cum[x-1][y-1] - cum[i][y-1] - cum[x-1][j])
+    p = [0] * (m+1)
+    for j in range(i, n+1):
+        t = [0] * (m+1)
+        for k in range(1, m+1):
+            p[k] += matrix[j][k]
+            t[k] = max(t[k-1] + p[k], p[k])
+            res = max(t[k], res)
 
 print(res)
