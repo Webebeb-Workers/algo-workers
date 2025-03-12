b0a69408d119d290e6f89a846b569bc865c599c1
유미라 <yoomira@yoomira-MacBookAir.local>
2025-03-12 15:24:13 +0900

    Refactor 지게차와 크레인

--- Programmers/250312_지게차와_크레인/ssum1ra.py
+++ Programmers/250312_지게차와_크레인/ssum1ra.py
@@ -1,10 +1,8 @@
 from collections import deque
 
 def check(arr, i, j):
-    visited = set()
-    visited.add((i, j))
-    q = deque()
-    q.append((i, j))
+    q = deque([(i, j)])
+    visited = set([(i, j)])
     
     while q:
         sx, sy = q.popleft()
@@ -15,34 +13,33 @@ def check(arr, i, j):
                 q.append((cx, cy))
                 visited.add((cx, cy))
     
-    for a, b in visited:
-        if a == 0 or b == 0 or a == len(arr) - 1 or b == len(arr[0]) - 1:
+    for x, y in visited:
+        if x == 0 or y == 0 or x == len(arr) - 1 or y == len(arr[0]) - 1:
             return True
         
     return False
 
 def solution(storage, requests):
     answer = 0
-    
     arr = [list(storage[i]) for i in range(len(storage))]
     
     for request in requests:
-        cand = set()
+        temp = set()
         for i in range(len(arr)):
             for j in range(len(arr[i])):
                 if len(request) == 2 and request[0] == arr[i][j]:
-                    cand.add((i, j))
+                    temp.add((i, j))
                 elif len(request) == 1 and request == arr[i][j]:
                     if i == 0 or j == 0 or i == len(arr) - 1 or j == len(arr[i]) - 1:
-                        cand.add((i, j))
+                        temp.add((i, j))
                     else:
                         if (arr[i+1][j] == '' and check(arr, i+1, j)) or \
                             (arr[i][j+1] == '' and check(arr, i, j+1)) or \
                             (arr[i-1][j] == '' and check(arr, i-1, j)) or \
                             (arr[i][j-1] == '' and check(arr, i, j-1)):
-                                cand.add((i, j))
-        for a, b in cand:
-            arr[a][b] = ''
+                                temp.add((i, j))
+        for x, y in temp:
+            arr[x][y] = ''
     
     for i in range(len(arr)):
         for j in range(len(arr[i])):
