20c3495d2f6f3cc2b14d1a01204940c9ab2765f8
유미라 <yoomira@yoomira-MacBookAir.local>
2025-03-17 17:20:51 +0900

    Refactor 석유 시추

--- Programmers/250317_석유_시추/ssum1ra.py
+++ Programmers/250317_석유_시추/ssum1ra.py
@@ -1,56 +1,47 @@
-from collections import deque
+def label_areas(land):
+    rows, cols = len(land), len(land[0])
+    labels = [[0] * cols for _ in range(rows)]
+    area_sizes = {}
+    label = 1
+    
+    def dfs(x, y):
+        stack = [(x, y)]
+        size = 0
+        while stack:
+            cx, cy = stack.pop()
+            if labels[cx][cy] == 0:
+                labels[cx][cy] = label
+                size += 1
+                for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
+                    nx, ny = cx + dx, cy + dy
+                    if 0 <= nx < rows and 0 <= ny < cols and land[nx][ny] == 1 and labels[nx][ny] == 0:
+                        stack.append((nx, ny))
+        return size
+    
+    for i in range(rows):
+        for j in range(cols):
+            if land[i][j] == 1 and labels[i][j] == 0:
+                size = dfs(i, j)
+                area_sizes[label] = size
+                label += 1
+    
+    return labels, area_sizes
 
-def check(land):
-    temp = [[0] * len(land[0]) for _ in range(len(land))]
-    cnt = 1
-    for i in range(len(land)):
-        for j in range(len(land[i])):
-            if land[i][j] == 1 and temp[i][j] == 0:
-                q = deque()
-                q.append((i, j))
-                visited = set()
-                visited.add((i, j))
-                temp[i][j] = cnt
-    
-                while q:
-                    cx, cy = q.popleft()
-                    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                        nx = cx + dx
-                        ny = cy + dy
-                        if 0 <= nx < len(land) and 0 <= ny < len(land[0]) and land[nx][ny] == 1 and (nx, ny) not in visited:
-                            q.append((nx, ny))
-                            visited.add((nx, ny))
-                            temp[nx][ny] = cnt
-            cnt += 1
-    
-    return temp
 
 def solution(land):
-    answer = 0
+    labels, area_sizes = label_areas(land)
+    cols = len(land[0])
+    max_oil = 0
     
-    temp = check(land)
+    for col in range(cols):
+        seen = set()
+        total = 0
+        for row in range(len(land)):
+            if labels[row][col] > 0:
+                label = labels[row][col]
+                if label not in seen:
+                    seen.add(label)
+                    total += area_sizes[label]
+        max_oil = max(max_oil, total)
     
-    dict = {}
-    for i in range(len(temp)):
-        for j in range(len(temp[0])):
-            if temp[i][j] != 0:
-                if temp[i][j] not in dict:
-                    dict[temp[i][j]] = 1
-                else:
-                    dict[temp[i][j]] += 1
-                
-    
-    for i in range(len(temp[0])):
-        visited = set()
-        for j in range(len(temp)):
-            if temp[j][i] != 0 and temp[j][i] not in visited:
-                visited.add(temp[j][i])
-                
-        res = 0
-        for v in visited:
-            res += dict[v]
-            
-        if res > answer:
-            answer = res
-        
-    return answer
+    return max_oil
