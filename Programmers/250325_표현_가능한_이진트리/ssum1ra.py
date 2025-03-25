038ac19640e5c0210374c31288f53a2107dee21b
유미라 <yoomira@yoomira-MacBookAir.local>
2025-03-25 15:55:23 +0900

    Create 표현 가능한 이진트리

---
+++ Programmers/250325_표현_가능한_이진트리/ssum1ra.py
@@ -0,0 +1,27 @@
+def check(arr):
+    if len(arr) <= 1:
+        return True
+
+    mid = len(arr) // 2
+    root = arr[mid]
+
+    if root == '0':
+        return all(x == '0' for x in arr)
+
+    return check(arr[:mid]) and check(arr[mid + 1:])
+
+def solution(numbers):
+    answer = []
+
+    for number in numbers:
+        b = list(bin(number))[2:]
+
+        l = 1
+        while l < len(b):
+            l = l * 2 + 1
+
+        b = ['0'] * (l - len(b)) + b[:]
+
+        answer.append(1 if check(b) else 0)
+
+    return answer
