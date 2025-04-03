102b59a197f254e0cf7ce84311b1073208415bdb
유미라 <yoomira@yoomira-MacBookAir.local>
2025-04-03 22:06:07 +0900

    Refactor 표 편집

--- Programmers/250403_표_편집/ssum1ra.py
+++ Programmers/250403_표_편집/ssum1ra.py
@@ -1,33 +1,42 @@
-from bisect import bisect_left
-
 def solution(n, k, cmd):
-    arr = list(range(n))
+    prev = [i - 1 for i in range(n)]
+    next = [i + 1 for i in range(n)]
+    next[n - 1] = -1
     stack = []
-    p = k
-    
+    cur = k
+
     for c in cmd:
-        temp = c.split()
-        if temp[0] == 'U':
-            p -= int(temp[1])
-        elif temp[0] == 'D':
-            p += int(temp[1])
-        elif temp[0] == 'C':
-            stack.append(arr[p])
-            arr = arr[:p] + arr[p+1:]
-            if p == len(arr):
-                p -= 1
-        elif temp[0] == 'Z':
-            num = stack.pop()
-            idx = bisect_left(arr, num)
-            if p >= idx:
-                p += 1
-            arr = arr[:idx] + [num] + arr[idx:]
-    
-    answer = []
-    for i in range(n):
-        if i in arr:
-            answer.append('O')
-        else:
-            answer.append('X')
-    
-    return ''.join(answer)
+        if c[0] == 'U':
+            x = int(c[2:])
+            for _ in range(x):
+                cur = prev[cur]
+                
+        elif c[0] == 'D':
+            x = int(c[2:])
+            for _ in range(x):
+                cur = next[cur]
+                
+        elif c[0] == 'C':
+            stack.append((cur, prev[cur], next[cur]))
+            if prev[cur] != -1:
+                next[prev[cur]] = next[cur]
+            if next[cur] != -1:
+                prev[next[cur]] = prev[cur]
+                
+            if next[cur] != -1:
+                cur = next[cur]
+            else:
+                cur = prev[cur]
+                
+        elif c[0] == 'Z':
+            node, p, n_ = stack.pop()
+            if p != -1:
+                next[p] = node
+            if n_ != -1:
+                prev[n_] = node
+
+    result = ['O'] * n
+    for node, _, _ in stack:
+        result[node] = 'X'
+
+    return ''.join(result)
