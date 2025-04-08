3758b45b2f6d5c5d2d5f8f8832385988dcecd6cd
rhino-ty <okko8522@gmail.com>
2025-04-08 09:31:29 +0900

    Modify 표 편집
    - 연결리스트 사용해 삭제 명령어 최적화

--- Programmers/250403_표_편집/rhino-ty.js
+++ Programmers/250403_표_편집/rhino-ty.js
@@ -1,6 +1,7 @@
 // 명령어에 따른 구현: 표의 행을 선택, 삭제, 복구하는 4가지 명령어(U X, D X, C, Z)를 처리해야 함
 // 현재 선택된 행 추적, 삭제된 이력을 중첩으로 관리(가장 늦게 들어온 Z부터 복구 => 스택, 삭제될 게 없을 때 Z는 주어지지 않음), 삭제 시
 // 최종적으로 삭제되지 않은 행은 "O", 삭제된 행은 "X"로 표시된 문자열 반환
+// => 최적화 방법: 삭제 시 순회하는 것이 아닌, 포인터 연결만 변경할 수 있게 => 연결 리스트
 
 function solution(n, k, cmd) {
   let curIdx = k;
@@ -11,52 +12,73 @@ function solution(n, k, cmd) {
   // 스택: 삭제 중첩 관리
   const deleted = [];
 
+  // 각 행의 이전/다음 행 정보를 저장 (연결 리스트 개념)
+  const prevRows = Array(n);
+  const nextRows = Array(n);
+
+  // 연결 리스트 초기화
+  for (let i = 0; i < n; i++) {
+    prevRows[i] = i - 1; // 이전 행 (첫 행은 -1)
+    nextRows[i] = i + 1; // 다음 행 (마지막 행은 n)
+  }
+  nextRows[n - 1] = -1; // 마지막 행의 다음 행은 없음
+
   for (const command of cmd) {
     // U X
     if (command.startsWith('U')) {
       let x = parseInt(command.split(' ')[1]);
-      while (x > 0 && curIdx > 0) {
-        curIdx--;
-        if (table[curIdx]) {
-          x--;
-        }
+      while (x > 0 && prevRows[curIdx] !== -1) {
+        curIdx = prevRows[curIdx];
+        x--;
       }
     }
     // D X
     else if (command.startsWith('D')) {
       let x = parseInt(command.split(' ')[1]);
-      while (x > 0 && curIdx < n - 1) {
-        curIdx++;
-        if (table[curIdx]) {
-          x--;
-        }
+      while (x > 0 && nextRows[curIdx] !== -1) {
+        curIdx = nextRows[curIdx];
+        x--;
       }
     }
     // C
     else if (command === 'C') {
-      deleted.push(curIdx);
+      // 삭제할 행 정보 저장 (행 번호, 이전 행, 다음 행)
+      deleted.push([curIdx, prevRows[curIdx], nextRows[curIdx]]);
       table[curIdx] = false;
 
-      // 다음 존재하는 행 찾기
-      let nextRow = curIdx + 1;
-      while (nextRow < n && !table[nextRow]) {
-        nextRow++;
+      // 이전 행과 다음 행의 연결 업데이트
+      const prev = prevRows[curIdx];
+      const next = nextRows[curIdx];
+
+      if (prev !== -1) {
+        nextRows[prev] = next;
       }
 
-      // 다음 행이 없으면(마지막 행이었다면) 이전 행 찾기
-      if (nextRow === n) {
-        nextRow = curIdx - 1;
-        while (nextRow >= 0 && !table[nextRow]) {
-          nextRow--;
-        }
+      if (next !== -1) {
+        prevRows[next] = prev;
+        curIdx = next; // 다음 행 선택
+      } else {
+        curIdx = prev; // 마지막 행이면 이전 행 선택
       }
-
-      curIdx = nextRow;
     }
     // Z
     else if (command === 'Z') {
-      const restored = deleted.pop();
-      table[restored] = true;
+      // 가장 최근에 삭제된 행 복구
+      const [row, prev, next] = deleted.pop();
+      table[row] = true;
+
+      // 연결 정보 복구
+      if (prev !== -1) {
+        nextRows[prev] = row;
+      }
+
+      if (next !== -1) {
+        prevRows[next] = row;
+      }
+
+      // 행의 이전/다음 정보도 복구
+      prevRows[row] = prev;
+      nextRows[row] = next;
     }
   }
 
