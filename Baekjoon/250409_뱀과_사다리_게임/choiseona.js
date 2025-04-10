5f8e3e193e14cacffb29886ce5212c7a6688d0f5
choiseona <tjsdk617@naver.com>
2025-04-10 12:25:22 +0900

    Update 뱀과 사다리 게임

--- Baekjoon/250409_뱀과_사다리_게임/choiseona.js
+++ Baekjoon/250409_뱀과_사다리_게임/choiseona.js
@@ -24,11 +24,9 @@ const BFS = () => {
     if (current === 100) return count;
 
     for (let i = 1; i <= 6; i++) {
-      let next = current + i;
-      if (next > 100 || visited[next]) continue;
-
       // 사다리 또는 뱀 위치로 이동
-      next = board[next];
+      let next = board[current + i];
+      if (next > 100 || visited[next]) continue;
 
       visited[next] = true;
       queue.push([next, count + 1]);
