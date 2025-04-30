e80df8d5866fbf6ae9b1ce9745e4c22be6023896
AEU <aeuplayground@gmail.com>
2025-04-30 09:57:02 +0900

    Create 펠린드롬?

---
+++ Baekjoon/250429_펠린드롬/dbjoung.js
@@ -0,0 +1,42 @@
+let [N, nums, M, ...SE] = require("fs")
+  .readFileSync(0)
+  .toString()
+  .trim()
+  .split("\n");
+N = Number(N);
+nums = nums.split(" ").map(Number);
+M = Number(M);
+
+const pelSet = new Array(2001)
+  .fill(null)
+  .map(() => new Array(2001).fill(false));
+
+// 홀수 펠린드롬
+for (let center = 0; center < nums.length; center++) {
+  let left = center;
+  let right = center;
+  while (left >= 0 && right < nums.length && nums[left] === nums[right]) {
+    pelSet[left + 1][right + 1] = true;
+    left--;
+    right++;
+  }
+}
+
+// 짝수 펠린드롬
+for (let center = 0; center < nums.length - 1; center++) {
+  let left = center;
+  let right = center + 1;
+  while (left >= 0 && right < nums.length && nums[left] === nums[right]) {
+    pelSet[left + 1][right + 1] = true;
+    left--;
+    right++;
+  }
+}
+
+const result = [];
+for (const se of SE) {
+  const [S, E] = se.split(" ").map(Number);
+  result.push(pelSet[S][E] ? 1 : 0);
+}
+
+console.log(result.join("\n"));
