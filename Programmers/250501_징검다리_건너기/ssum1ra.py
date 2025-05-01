c0c9dcda68d0bac422af4340dc2017918181c2d2
유미라 <yoomira@yoomira-MacBookAir.local>
2025-05-01 16:04:18 +0900

    Create 징검다리 건너기

---
+++ Programmers/250501_징검다리_건너기/ssum1ra.py
@@ -0,0 +1,21 @@
+def solution(stones, k):
+    l = min(stones)
+    r = max(stones)
+    while l <= r:
+        m = (l+r) // 2 # 친구들의 수
+        
+        cnt = 0 # 건너뛴 디딤돌의 개수
+        for s in stones:
+            if s <= m:
+                cnt += 1
+                if cnt >= k:
+                    break
+            else:
+                cnt = 0
+        
+        if cnt >= k:
+            r = m - 1
+        else:
+            l = m + 1
+            
+    return l
