edd1f3c15962774356783d88b2beec0bf35bdcc8
유미라 <yoomira@yoomira-MacBookAir.local>
2025-05-20 17:51:49 +0900

    Refactor 파일 합치기

--- Baekjoon/250520_파일_합치기/ssum1ra.py
+++ Baekjoon/250520_파일_합치기/ssum1ra.py
@@ -6,17 +6,26 @@ for _ in range(T):
     K = int(input())
     C = list(map(int, input().split()))
 
-    prefix_sum = [0] * (K+1)
-    for i in range(1, K+1):
-        prefix_sum[i] = prefix_sum[i-1] + C[i-1]
+    prefix_sum = [0] * (K + 1)
+    for i in range(1, K + 1):
+        prefix_sum[i] = prefix_sum[i - 1] + C[i - 1]
 
-    dp = [[0] * (K+1) for _ in range(K+1)]
+    dp = [[0] * (K + 1) for _ in range(K + 1)]
+    opt = [[0] * (K + 1) for _ in range(K + 1)]
 
-    for l in range(2, K+1):
-        for i in range(1, K - l + 2):
-            j = i + l - 1
+    for i in range(1, K + 1):
+        opt[i][i] = i
+
+    for length in range(2, K + 1):
+        for i in range(1, K - length + 2):
+            j = i + length - 1
             dp[i][j] = float('inf')
-            for k in range(i, j):
-                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + (prefix_sum[j] - prefix_sum[i-1]))
+            for k in range(opt[i][j - 1], opt[i + 1][j] + 1):
+                if k >= j:
+                    continue
+                cost = dp[i][k] + dp[k + 1][j] + (prefix_sum[j] - prefix_sum[i - 1])
+                if cost < dp[i][j]:
+                    dp[i][j] = cost
+                    opt[i][j] = k
 
     print(dp[1][K])
