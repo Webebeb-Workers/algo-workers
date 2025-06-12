e647e7611d3a585697d8cd9ab0ce4a609ca5dbc0
choiseona <tjsdk617@naver.com>
2025-06-12 12:14:32 +0900

    Create 기능 개발

---
+++ Programmers/250610_기능개발/choiseona.js
@@ -0,0 +1,17 @@
+function solution(progresses, speeds) {
+  const answer = [0];
+  const remainDays = progresses.map((e, i) => Math.ceil((100 - progresses[i]) / speeds[i]));
+
+  let maxRemainDay = remainDays[0];
+
+  for (let i = 0, j = 0; i < remainDays.length; i++) {
+    if (remainDays[i] > maxRemainDay) {
+      maxRemainDay = remainDays[i];
+      answer[++j] = 0;
+    }
+    if (remainDays[i] <= maxRemainDay) {
+      answer[j]++;
+    }
+  }
+  return answer;
+}
